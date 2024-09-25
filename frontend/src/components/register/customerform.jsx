1c9c0cabcf022c4221dbbb9264a2d959f7a46755
toby77777777 <kibichiitoby4@gmail.com>
2024-09-25 20:26:00 +0300

    drilling userAccount property to the different forms

---
+++ frontend/src/components/register/customerform.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";  
+
+export const CustomerForm = ({ userAccount }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/register", {
+        email,
+        password,
+      });
+      alert(res.data.message);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        defaultValue={userAccount}
+      /> 
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button type="submit">register</button>
+    </form>
+  );
+};
---
+++ frontend/src/components/register/register.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { CustomerForm } from "./customerform";
+import { SellerForm } from "./sellerform";
+
+export const Register = () => {
+  const [userAccount, setUserAccount] = useState("customer");
+
+  const handleAccountChange = (event) => {
+    setUserAccount(event.target.value); 
+
+  };
+
+
+  return (
+    <div>
+      <h1>Create Your Account</h1>
+      <select
+        name="useraccount"
+        value={userAccount}
+        onChange={handleAccountChange}
+      >
+        <option value="customer">Customer</option>
+        <option value="seller">Seller</option>
+      </select>
+      {userAccount === "seller" ? (
+        <SellerForm userAccount={userAccount} />
+      ) : (
+        <CustomerForm userAccount={userAccount} />
+      )}
+    </div>
+  );
+};
