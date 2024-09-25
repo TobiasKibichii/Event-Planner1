da4580c7ca39956cc44f3db5863c000b897ce854
toby77777777 <kibichiitoby4@gmail.com>
2024-09-25 22:02:03 +0300

    reduced the routes in App.jsx by modiying login and register routes to be handled differently in home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { Login } from "./components/login";
-import { Register } from "./components/register/register";
 import { Home } from "./components/home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export const App = () => {
@@ -7,8 +5,6 @@ export const App = () => {
     <Router> 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Login } from "./login"; 
+import { Register } from "./register/register"; 
 
-export const Home =()=> {
-    return (
-        <div>
-            <h1>Welcome to my home page</h1>
-            <Link to={'/login'}>login
-            </Link>
-            <Link to={'/register'}>register
-            </Link>
-        </div>
-        )
-}
+export const Home = () => {
+  
+  const [showForm, setShowForm] = useState(null);
+
+  return (
+    <div>
+      <h1>Welcome to my home page</h1>
+
+      
+      <button onClick={() => setShowForm("login")}>Login</button>
+
+      <button onClick={() => setShowForm("register")}>Register</button>
+
+      {showForm === "login" && <Login />}
+      {showForm === "register" && <Register />}
+    </div>
+  );
+};
